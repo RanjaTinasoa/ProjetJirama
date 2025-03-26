05958051dfa5b30b5a1d6d9ec6a3790bd17bb36e
ekena <ekenarakotoson16@gmail.com>
2025-03-26 14:18:03 +0100

    'modify-facture-Model'

---
+++ controllers/factureController.php
@@ -0,0 +1,10 @@
+<?php
+require_once __DIR__ ."/../models/facture.php";
+require_once __DIR__ ."/../models/user.php";
+class Facture{
+    public static function viewFacture($codecli){
+        
+        #$datas = array($mois,$nom,$date_presentation,$date_limite_paie,$quartier,$codecompteur);
+    }
+    
+}
--- models/facture.php
+++ models/facture.php
@@ -35,7 +35,7 @@ class FactureModel{
     }
     public function get_valeur_eau($codecli){
         $sql = "SELECT eau.valeur2 FROM releve_eau eau INNER JOIN compteur compt ON compt.codecompteur=eau.codecompteur
-         where compt.codecli='$codecli' AND compt.type='eau'";
+         where compt.codecli='$codecli' AND compt.type='eau' order by eau.date_limite_paie2 desc limit3";
         if ($this->db->query($sql)->fetch_all()){
             $result = $this->db->query($sql)->fetch_all();
             $result = $result[0][0];
@@ -47,7 +47,7 @@ class FactureModel{
     }
     public function get_valeur_elec($codecli){
         $sql = "SELECT elec.valeur1 FROM releve_elec elec INNER JOIN compteur compt ON compt.codecompteur=elec.codecompteur
-         where compt.codecli='$codecli' AND compt.type='electricite'";
+         where compt.codecli='$codecli' AND compt.type='electricite' order by elec.date_limite_paie desc limit3";
         if ($this->db->query($sql)->fetch_all()){
             $result = $this->db->query($sql)->fetch_all();
             $result = $result[0][0];
@@ -60,7 +60,7 @@ class FactureModel{
     public function get_total_releve_elec($codecli){
         $sql = "SELECT relec.valeur1*compt.pu as total from releve_elec relec INNER JOIN compteur compt 
         ON compt.codecompteur=relec.codecompteur INNER JOIN client cli on cli.codecli=compt.codecli
-        where cli.codecli='$codecli' and compt.type='electricite'";
+        where cli.codecli='$codecli' and compt.type='electricite' order by relec.date_limite_paie desc limit3";
         if ($this->db->query($sql)->fetch_all()){
         $result = $this->db->query($sql)->fetch_all();
         $res = $result[0][0];
@@ -74,9 +74,9 @@ class FactureModel{
     
 
     public function get_total_releve_eau($codecli){
-        $sql = "SELECT relec.valeur2*compt.pu as total from releve_eau relec INNER JOIN compteur compt 
-        ON compt.codecompteur=relec.codecompteur INNER JOIN client cli on cli.codecli=compt.codecli
-        where cli.codecli='$codecli' and compt.type='eau' ";
+        $sql = "SELECT reau.valeur2*compt.pu as total from releve_eau reau INNER JOIN compteur compt 
+        ON compt.codecompteur=reau.codecompteur INNER JOIN client cli on cli.codecli=compt.codecli
+        where cli.codecli='$codecli' and compt.type='eau' order by date_limite_paie2 desc limit3";
 
         
     if ($this->db->query($sql)->fetch_all()){
@@ -88,6 +88,12 @@ class FactureModel{
     }
     return $res;
 }
+    public function getDatas($codecli){
+        $sql = "SELECT c.nom, c.codecli, c.quartier, r.date_presentation,r.date_limite_paie,r.codecompteur,reau.codecompteur from compteur join client c on c.codecli=compteur.codecli
+         join releve_eau reau on reau.codecompteur=compteur.codecompteur join releve_elec r on r.codecompteur=compteur.codecompteur where
+          codecli='$codecli' order by date_limite_paie desc limit 3";
+          $result = $this->db->query($sql)->fetch_all();
+    }
 }
 
 
